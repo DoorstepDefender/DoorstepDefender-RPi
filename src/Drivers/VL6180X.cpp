a3a19a6d046fb7e7a5d745127eb99a49b03ad4d0
Peter Lilley <hi.petelilley@gmail.com>
2023-05-12 10:12:12 -0500

    Worked on VL6180X driver

--- src/Drivers/VL6180X.cpp
+++ src/Drivers/VL6180X.cpp
@@ -29,13 +29,21 @@ VL6180X::VL6180X(int adapter_nr, uint8_t addr) {
 VL6180X::~VL6180X() {
 }
 
+double VL6180X::get_range() {
+  
+}
+
 uint8_t VL6180X::readRegister(uint16_t reg) {
+  if (!connected) return 0;
+  
   uint8_t buffer[3];
   buffer[0] = uint8_t(reg >> 8);
   buffer[1] = uint8_t(reg & 0xFF);
 
+  // Prompt the sensor with the address to read.
   write(i2c_fd, buffer, 2);
 
+  // Read the value at the address.
   bool res = read(i2c_fd, &buffer[2], 1);
   if (res) {
     fmt::print("Failed to read from register {::#x}\n", reg);
@@ -44,12 +52,15 @@ uint8_t VL6180X::readRegister(uint16_t reg) {
 }
 
 void VL6180X::writeRegister(uint16_t reg, uint8_t data) {
+  if (!connected) return;
+  
   uint8_t buffer[3];
 
   buffer[0] = uint8_t(reg >> 8);
   buffer[1] = uint8_t(reg & 0xFF);
   buffer[2] = data;
 
+  // Write data to register at address.
   if (write(i2c_fd, buffer, 3) != 3) {
     fmt::print("Failed to write to register {::#x}\n", reg);
   }

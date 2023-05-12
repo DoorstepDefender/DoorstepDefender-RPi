3cae54b434197020f8c88dc98d61a348a95c85c1
Peter Lilley <hi.petelilley@gmail.com>
2023-05-12 10:08:01 -0500

    Added comments to VL6180X driver

--- include/DoorstepDefender/Drivers/VL6180X.h
+++ include/DoorstepDefender/Drivers/VL6180X.h
@@ -15,13 +15,41 @@ public:
   VL6180X(int adapter_nr, uint8_t addr = DEFAULT_ADDR);
   ~VL6180X();
 
+  /**
+   * @brief Returns whether the device is connected.
+   *
+   * @return Whether the device is connected.
+   */
   constexpr bool is_connected() const { return connected; }
 
+  /**
+   * @brief Returns the range reading of the sensor (cm).
+   *
+   * @return The range reading of the sensor (cm).
+   */
+  double get_range();
+  
 private:
+  
+  /**
+   * @brief Reads an 8-bit value from a 16-bit register location over I2C.
+   *
+   * @param reg The 16-bit register address.
+   * @return The 8-bit register value.
+   */
   uint8_t readRegister(uint16_t reg);
+  
+  /**
+   * @brief Writes an 8-bit value to a 16-bit register location over I2C.
+   *
+   * @param reg The 16-bit register address.
+   * @param data The 8-bit register value.
+   */
   void writeRegister(uint16_t reg, uint8_t data);
 
+  // File descriptor of the I2C device.
   int i2c_fd;
 
+  // Whether the device is connected.
   bool connected;
 };
